06f2f1badedb2614710a0fd535e25044e81ef0b3
rafbatista <rah5024@gmail.com>
2018-03-14 23:27:27 -0700

    Created playSound function. #1

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-window.addEventListener('keydown', event => {
+const playSound = event => {
+  console.log(audio)
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
   const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
   if (!audio) return
-})
+}
+window.addEventListener('keydown', playSound)
