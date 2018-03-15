ed8952a00cb2ecfc01a6d024f257101891915ae8
rafbatista <rah5024@gmail.com>
2018-03-14 23:00:54 -0700

    Added eventListener for keying down. #1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function play(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+window.addEventListener('keydown', event => {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
   if (!audio) return
+})
