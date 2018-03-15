e43a48e6c92abc12810f93575aebcf8fa08859be
rafbatista <rah5024@gmail.com>
2018-03-14 23:43:08 -0700

    User can hear a sound when playing a key. #1

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const playSound = event => {
-  console.log(audio)
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
   const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
   if (!audio) return
+  audio.currentTime = 0
+  audio.play()
 }
+
 window.addEventListener('keydown', playSound)
